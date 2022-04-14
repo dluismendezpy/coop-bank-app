a6b5bde054fe5f6b3815cae659b79f16c10a9cf9
Luis Mendez <dluismendezdj@gmail.com>
2022-04-14 10:34:50 -0400

    Add createAppContainer and createDrawerNavigator

--- App.jsx
+++ App.jsx
@@ -1,20 +1,48 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { HomeScreen, NewsScreen } from "./src/components/Navigation";
+import Sidebar from "./src/components/Sidebar";
+import { Feather } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { Dimensions } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+const DrawerNavigator = createDrawerNavigator(
+  {
+    HomeScreen: {
+      screen: HomeScreen,
+      navigationOptions: {
+        title: "Home",
+        drawerIcon: ({ tintColor }) => (
+          <Feather name="home" size={16} color={tintColor} />
+        ),
+      },
+    },
+    NewsScreen: {
+      screen: NewsScreen,
+      navigationOptions: {
+        title: "News",
+        drawerIcon: ({ tintColor }) => (
+          <Entypo name="news" size={16} color={tintColor} />
+        ),
+      },
+    },
   },
-});
+  {
+    contentComponent: (props) => <Sidebar {...props} />,
+    drawerWidth: Dimensions.get("window").width * 0.75,
+    hideStatusBar: true,
+    contentOptions: {
+      activeBackgroundColor: "rgba(121,118,212,0.2)",
+      activeTintColor: "#53115B",
+      itemsContainerStyle: {
+        marginTop: 16,
+        marginHorizontal: 8,
+      },
+      itemStyle: {
+        borderRadius: 4,
+      },
+    },
+  }
+);
+
+export default createAppContainer(DrawerNavigator);
