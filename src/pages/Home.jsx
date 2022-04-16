7fe79d19e1dfa165835bff671e3d33a707e7b02d
Luis Mendez <dluismendezdj@gmail.com>
2022-04-16 11:15:11 -0400

    Create home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,35 +2,125 @@ import React from "react";
 import {
   View,
   Text,
-  StyleSheet,
-  SafeAreaView,
   TouchableOpacity,
+  Dimensions,
+  StyleSheet,
+  StatusBar,
+  Image,
 } from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
+import * as Animatable from "react-native-animatable";
+import { LinearGradient } from "expo-linear-gradient";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { useTheme } from "react-native-paper";
+import {FontAwesome5} from "@expo/vector-icons";
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <TouchableOpacity
-          style={{ alignItems: "flex-start", margin: 10, marginLeft: 20 }}
-          onPress={() => this.props.navigation.openDrawer()}
+const SplashScreen = ({ navigation }) => {
+  const { colors } = useTheme();
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={{ alignItems: "flex-start", margin: 10, marginLeft: 20 }}
+        onPress={() => navigation.openDrawer()}
+      >
+        <FontAwesome5 name={"bars"} size={30} color="#000000" />
+      </TouchableOpacity>
+      <StatusBar backgroundColor="#009387" barStyle="light-content" />
+      <View style={styles.header}>
+        <Animatable.Image
+          animation="bounceIn"
+          duraton="1500"
+          source={require("../../assets/Home/logo.png")}
+          style={styles.logo}
+          resizeMode="stretch"
+        />
+      </View>
+      <Animatable.View
+        style={[
+          styles.footer,
+          {
+            backgroundColor: colors.background,
+          },
+        ]}
+        animation="fadeInUpBig"
+      >
+        <Text
+          style={[
+            styles.title,
+            {
+              color: colors.text,
+            },
+          ]}
         >
-          <FontAwesome5 name={"bars"} size={30} color="#000000" />
-        </TouchableOpacity>
-        <View style={styles.container}>
-          <Text>Home side</Text>
+          Stay connected with everyone!
+        </Text>
+        <Text style={styles.text}>Sign in with account</Text>
+        <View style={styles.button}>
+          <TouchableOpacity onPress={() => navigation.navigate("SignInScreen")}>
+            <LinearGradient
+              colors={["#08d4c4", "#01ab9d"]}
+              style={styles.signIn}
+            >
+              <Text style={styles.textSign}>Get Started</Text>
+              <MaterialIcons name="navigate-next" color="#fff" size={20} />
+            </LinearGradient>
+          </TouchableOpacity>
         </View>
-      </SafeAreaView>
-    );
-  }
-}
+      </Animatable.View>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const { height } = Dimensions.get("screen");
+const height_logo = height * 0.28;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    backgroundColor: "#009387",
+  },
+  header: {
+    flex: 2,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  footer: {
+    flex: 1,
     backgroundColor: "#fff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingVertical: 50,
+    paddingHorizontal: 30,
+  },
+  logo: {
+    width: height_logo,
+    height: height_logo,
+  },
+  title: {
+    color: "#05375a",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  text: {
+    color: "grey",
+    marginTop: 5,
+  },
+  button: {
+    alignItems: "flex-end",
+    marginTop: 30,
+  },
+  signIn: {
+    width: 150,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    flexDirection: "row",
+  },
+  textSign: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
