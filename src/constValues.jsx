f056f652d9e11d6267b0fec34317fa57ca3b173f
Luis Mendez <dluismendezdj@gmail.com>
2022-04-25 14:53:16 -0400

    Disable warning messages

--- src/constValues.jsx
+++ src/constValues.jsx
@@ -3,3 +3,4 @@ export const storageTokenKey = "@token:value";
 export const firstNameUserKey = "@key:valueFirstName";
 export const lastNameUserKey = "@key:valueLastName";
 export let isLoggedIn = true;
+console.disableYellowBox = true;
