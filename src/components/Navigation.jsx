12b75f3b3a0b01ba98ff790dbe42d80f4eb36c68
Luis Mendez <dluismendezdj@gmail.com>
2022-04-25 15:34:16 -0400

    Signup code refactor

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Home from "../pages/Home";
 import News from "../pages/News";
 import Login from "../pages/Login";
-import signup from "../pages/Signup";
+import signup from "../pages/Signup/Signup";
 import Feed from "../pages/Feed";
 import ClearData from "../Helpers/ClearData";
 import HelpCenter from "../pages/HelpCenter";
 import ForgotPassword from "./ForgotPassword";
-import Signup from "../pages/Signup";
+import Signup from "../pages/Signup/Signup";
 
 export const HomeScreen = ({ navigation }) => <Home navigation={navigation} />;
 export const NewsScreen = ({ navigation }) => <News navigation={navigation} />;
---
+++ src/pages/Signup/strings.jsx
@@ -0,0 +1,17 @@
+export const strings = {
+  emptyCredentials: "Cédula o contraseña no pueden estar en blanco.",
+  tryAgain: "Reintentar",
+  ohRight: "Entendido",
+  error: "Error",
+  haveAnAccount: "¿Tienes una Cuenta?",
+  next: "Siguiente",
+  troubleSigningIn: "¿Problemas para registrarte?",
+  password: "Contraseña.",
+  passwordWarningMessage: "La contraseña debe tener 3 caracteres.",
+  usernameWarningMessage: "El usuario debe tener 3 caracteres.",
+  signUp: "Registrate",
+  identificationNumber: "Cédula",
+  warning: "Aviso",
+  messageAfterSignup: "Pronto seras contactado por uno de nuestros asistentes.",
+  access: "Entrar"
+};
